51716c90310935282707454b7871c8c1b62d3958
Michał Cholewiński <cholewinskimichal@gmail.com>
2017-09-19 19:33:48 +0200

    Added Html Hot Module Reloader

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,26 @@
 var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+var HtmlWebPackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-	entry: "./entry.js",
-	output: {
-		path: __dirname,
-		filename: "bundle.js"
-	},
-	module: {
-		loaders: [
-			{ test: /\.css$/, loader: "style-loader!css" }
-		]
-	},
-	plugins: [
-		new BrowserSyncPlugin({
-			host: 'localhost',
-			port: 3000,
-			proxy: 'http://localhost:8080/'
-		})
-	]
+    entry: "./entry.js",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            {test: /\.css$/, loader: "style-loader!css"},
+            {test: /\.html$/, loader: "raw-loader"}
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+           template: 'index.html'
+        }),
+        new BrowserSyncPlugin({
+            host: 'localhost',
+            port: 3000,
+            proxy: 'http://localhost:8080/'
+        })
+    ]
 };
