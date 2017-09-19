3d46bf91466137d923788fb027a2bb5ad2593914
Michał Cholewiński <cholewinskimichal@gmail.com>
2017-09-19 23:54:17 +0200

    Using CSS Gradients

--- entry.js
+++ entry.js
@@ -1,6 +1,1 @@
 require("!style-loader!css-loader!./style.css");
-var f = document.getElementById('foo');
-document.addEventListener('click', function(ev){
-    f.style.transform = 'translateY('+(ev.clientY-25)+'px)';
-    f.style.transform += 'translateX('+(ev.clientX-25)+'px)';
-},false);
